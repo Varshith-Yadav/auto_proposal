9ea161148d428b8b064353f94901faa64b61a2d4
Varshith-Yadav <varshithlavanya67@gmail.com>
2026-03-21 17:42:32 +0530

    updated scripts

--- scripts/run_ingestion.py
+++ scripts/run_ingestion.py
@@ -1,4 +1,10 @@
-from ..ingestion.pipeline import run_ingestion
+from pathlib import Path
+import sys
+
+ROOT = Path(__file__).resolve().parents[1]
+sys.path.append(str(ROOT))
+
+from ingestion.pipeline import run_ingestion
 
 if __name__ == "__main__":
     run_ingestion()
